173909472192e9b0c4658e05bbce18569778343c
Shoichi Sakane <github-shoichi@tanu.org>
2017-09-23 21:36:16 +0900

    enabled to specify the all parameters for the fomula to caculate the ToA.

--- lorawan_toa.py
+++ lorawan_toa.py
@@ -15,21 +15,36 @@ def mpsrange(a, b):
     b += 6  # MHDR + MIC + 1
     return range(a, b)
 
-def get_toa(n_size, n_sf, bw=125, de=0):
+def get_toa(n_size, n_sf, n_bw=125, enable_dro=1, disable_h=0, n_cr=1,
+            n_preamble=8):
     '''
-    n_size: PHY Payload size (MAC Payload)
+    n_size:
+        PL in the fomula.  PHY Payload size in byte (= MAC Payload + 5)
     n_sf: SF (12 to 7)
-    bw: Bandwidth.  default is 125 KHz.
+    n_bw: Bandwidth in kHz.  default is 125 kHz for AS923.
+    enable_dro:
+        DE in the fomula.
+        1 when the low data rate optimization is enabled.
+        0 for disabled.
+        LoRaWAN always use the low data optimization.
+    disable_eh:
+        H in the fomula.
+        0 when the explicit header is enabled.
+        1 when no header is present.
+        LoRaWAN always use the header.
+    n_cr:
+        CR in the fomula, should be from 1 to 4.
+        Coding Rate = (n_cr/(n_cr+1)).
+        LoRaWAN takes alway 1.
+    n_preamble:
+        The preamble length in bit.
+        default is 8 in AS923.
     '''
-    t_sym = math.pow(2, n_sf) / (bw*1000.) * 1000.
-    n_preamble = 8    # AS923
+    t_sym = math.pow(2, n_sf) / (n_bw*1000.) * 1000.
     t_preamble = (n_preamble + 4.25) * t_sym
-    v_h = 0    # LoRaWAN: No explicit header
-    v_de = de  # LoRaWAN: low data optimization
-    v_cr = 1   # LoRaWAN: 4/5
-    a = 8.*n_size - 4.*n_sf + 28 + 16 - 20.*v_h
-    b = 4.*(n_sf-2.*v_de)
-    nb_sym_payload = 8 + max(math.ceil(a/b)*(v_cr+4), 0)
+    a = 8.*n_size - 4.*n_sf + 28 + 16 - 20.*disable_h
+    b = 4.*(n_sf-2.*enable_dro)
+    nb_sym_payload = 8 + max(math.ceil(a/b)*(n_cr+4), 0)
     t_payload = nb_sym_payload * t_sym
     t_frame = t_preamble+ t_payload
 
@@ -37,20 +52,57 @@ def get_toa(n_size, n_sf, bw=125, de=0):
 
 if __name__ == "__main__" :
     import sys
-    if len(sys.argv) < 3:
-        print("Usage: %s (sf) (size) [bw] [de]" % (sys.argv[0]))
-        print("    size: PHY Payload siz (= MAC Payload + 5)")
-        print("    bw: bandwidth. default is 125 kHz.")
-        print("    de: low data optimization. default is 0.")
-        exit(1)
-    n_sf = int(sys.argv[1])
-    n_size = int(sys.argv[2])
-    n_bw = 125
-    n_de = 0
-    if len(sys.argv) == 4:
-        n_bw = int(sys.argv[3])
-    elif len(sys.argv) == 5:
-        n_bw = int(sys.argv[3])
-        n_de = int(sys.argv[4])
-    t_frame = get_toa(n_size, n_sf, bw=n_bw, de=n_de)
-    print "%.3f (ms)" % t_frame
+    import argparse
+
+    def parse_args():
+        p = argparse.ArgumentParser(
+                description="LoRa Time on Air calculator.",
+                epilog="")
+        p.add_argument("n_sf", metavar="SF", type=int,
+            help="Spreading Factor. It should be from 7 to 12.")
+        p.add_argument("n_size", metavar="SIZE", type=int,
+            help="PHY payload size in byte. It's equal to the MAC payload + 5.")
+        p.add_argument("--band-width", action="store", dest="n_bw", type=int,
+            default=125,
+            help="bandwidth in kHz. default is 125 kHz.")
+        p.add_argument("--disable-dro", action="store_const", dest="v_de",
+            const=0, default=1,
+            help="disable the low data rate optimization. default is enable as LoRaWAN does.")
+        p.add_argument("--disable-eh", action="store_const", dest="v_h",
+            const=1, default=0,
+            help="disable the explicit header.  default is enable as LoRaWAN does.")
+        p.add_argument("--cr", action="store", dest="n_cr", type=int, default=1,
+            help="specify the CR value. default is 1 as LoRaWAN does.")
+        p.add_argument("--preamble", action="store", dest="n_preamble",
+            type=int, default=8,
+            help="specify the preamble. default is 8 for AS923.")
+        p.add_argument("-v", action="store_true", dest="f_verbose",
+            default=False,
+            help="enable verbose mode.")
+        p.add_argument("-d", action="append_const", dest="_f_debug", default=[],
+            const=1, help="increase debug mode.")
+    
+        args = p.parse_args()
+        args.debug_level = len(args._f_debug)
+        return args
+
+    #
+    # main
+    #
+    opt = parse_args()
+    t_frame = get_toa(opt.n_size, opt.n_sf,
+                      n_bw=opt.n_bw, enable_dro=opt.v_de,
+                      disable_h=opt.v_h, n_cr=opt.n_cr,
+                      n_preamble=opt.n_preamble)
+    if opt.f_verbose:
+        print "PHY payload size  : %d Bytes" % opt.n_size
+        print "MAC payload size  : %d Bytes" % (opt.n_size-5)
+        print "Spreading Factor  : %d" % opt.n_sf
+        print "Band width        : %d kHz" % opt.n_bw
+        print "Low data rate opt.: %s" % ("enable" if opt.v_de else "disable")
+        print "Explicit header   : %s" % ("disable" if opt.v_h else "enable")
+        print "CR (coding rate)  : %d (4/%d)" % (opt.n_cr, 4+opt.n_cr)
+        print "Preamble size     : %d symbols" % opt.n_preamble
+        print "Time on Air       : %.3f msec" % t_frame
+    else:
+        print "%.3f" % t_frame
--- test.py
+++ test.py
@@ -10,16 +10,16 @@ class TestUM(unittest.TestCase):
         pass
 
     def test_1(self):
-        self.assertEqual( get_toa(64, 12, bw=125, de=0), 2465.792000 )
+        self.assertEqual( get_toa(64, 12, n_bw=125, enable_dro=0), 2465.792000 )
 
     def test_2(self):
-        self.assertEqual( get_toa(64, 12, bw=125, de=1), 2793.472000 )
+        self.assertEqual( get_toa(64, 12, n_bw=125, enable_dro=1), 2793.472000 )
 
     def test_3(self):
-        self.assertEqual( get_toa(255, 7, bw=500, de=0), 99.904000 )
+        self.assertEqual( get_toa(255, 7, n_bw=500, enable_dro=0), 99.904000 )
 
     def test_4(self):
-        self.assertEqual( get_toa(255, 7, bw=500, de=1), 137.024 )
+        self.assertEqual( get_toa(255, 7, n_bw=500, enable_dro=1), 137.024 )
 
 if __name__ == '__main__':
     unittest.main()
