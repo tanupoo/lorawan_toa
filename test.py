4616bf647257f311bb94960c65d0c425f51bd12c
Shoichi Sakane <github-shoichi@tanu.org>
2017-10-08 13:36:35 +0900

    aligh to the change of the return value.

--- test.py
+++ test.py
@@ -10,16 +10,20 @@ class TestUM(unittest.TestCase):
         pass
 
     def test_1(self):
-        self.assertEqual( get_toa(64, 12, n_bw=125, enable_dro=0), 2465.792000 )
+        toa = get_toa(64, 12, n_bw=125, enable_dro=0)["t_packet"]
+        self.assertEqual( toa, 2465.792000 )
 
     def test_2(self):
-        self.assertEqual( get_toa(64, 12, n_bw=125, enable_dro=1), 2793.472000 )
+        toa = get_toa(64, 12, n_bw=125, enable_dro=1)["t_packet"]
+        self.assertEqual( toa, 2793.472000 )
 
     def test_3(self):
-        self.assertEqual( get_toa(255, 7, n_bw=500, enable_dro=0), 99.904000 )
+        toa = get_toa(255, 7, n_bw=500, enable_dro=0)["t_packet"]
+        self.assertEqual( toa, 99.904000 )
 
     def test_4(self):
-        self.assertEqual( get_toa(255, 7, n_bw=500, enable_dro=1), 137.024 )
+        toa = get_toa(255, 7, n_bw=500, enable_dro=1)["t_packet"]
+        self.assertEqual( toa, 137.024 )
 
 if __name__ == '__main__':
     unittest.main()
