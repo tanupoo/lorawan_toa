4a37bc82c72ecb5fc2cf0e8b24d4eda5d0d131df
Shoichi Sakane <github-shoichi@tanu.org>
2017-10-09 14:42:40 +0900

    added new test set, e.g. auto LDRO.

--- test.py
+++ test.py
@@ -9,21 +9,38 @@ class TestUM(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_1(self):
-        toa = get_toa(64, 12, n_bw=125, enable_dro=0)["t_packet"]
-        self.assertEqual( toa, 2465.792000 )
+    def test_11(self):
+        toa = get_toa(32, 12)["t_packet"]
+        self.assertEqual( toa, 1810.432 )
 
-    def test_2(self):
-        toa = get_toa(64, 12, n_bw=125, enable_dro=1)["t_packet"]
-        self.assertEqual( toa, 2793.472000 )
+    def test_12(self):
+        toa = get_toa(32, 11)["t_packet"]
+        self.assertEqual( toa, 987.136 )
 
-    def test_3(self):
-        toa = get_toa(255, 7, n_bw=500, enable_dro=0)["t_packet"]
-        self.assertEqual( toa, 99.904000 )
+    def test_13(self):
+        toa = get_toa(32, 12, n_bw=250)["t_packet"]
+        self.assertEqual( toa, 905.216 )
 
-    def test_4(self):
-        toa = get_toa(255, 7, n_bw=500, enable_dro=1)["t_packet"]
-        self.assertEqual( toa, 137.024 )
+    def test_14(self):
+        toa = get_toa(32, 12, n_bw=125, enable_auto_ldro=False, enable_ldro=True)["t_packet"]
+        self.assertEqual( toa, 1810.432 )
+
+    def test_21(self):
+        toa = get_toa(64, 12, enable_eh=False)["t_packet"]
+        self.assertEqual( toa, 2793.472 )
+
+    def test_22(self):
+        toa = get_toa(64, 12, enable_crc=False)["t_packet"]
+        self.assertEqual( toa, 2793.472 )
+
+    def test_31(self):
+        toa = get_toa(12, 7)["t_packet"]
+        self.assertEqual( toa, 41.216 )
+
+    def test_41(self):
+        toa = get_toa(12, 7, n_bw=500, enable_eh=False, enable_crc=False,
+                      n_cr=4, n_preamble=6)["t_packet"]
+        self.assertEqual( toa, 10.816 )
 
 if __name__ == '__main__':
     unittest.main()
